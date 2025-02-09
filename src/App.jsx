96b48d9a6d5084d9c95e69f10cd81690b2123098
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-02-09 23:20:20 +0100

    Alert Clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import HelloWorld from "./HelloWorld";
 import Welcome from "./Welcome";
+import AlertClock from "./AlertClock";
 
 const name = "Clara";
 const age = 24;
 
 export default function App() {
+  function handleButtonClick() {
+    const now = new Date();
+
+    alert(`The current time is ${now.toLocaleTimeString()}`);
+  }
   return (
     <div>
       <HelloWorld />
       <HelloWorld />
       <Welcome name={name} age={age} />
+      <AlertClock onClick={handleButtonClick} />
     </div>
   );
 }
